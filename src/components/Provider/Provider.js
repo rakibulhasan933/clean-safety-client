3b1ab8b113fdb90fcf07fdd48decdc09209e52f6
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-11 01:04:11 +0600

    overflow solved

--- src/components/Provider/Provider.js
+++ src/components/Provider/Provider.js
@@ -3,22 +3,24 @@ import './Provider.css'
 
 const Provider = () => {
     return (
-        <div className="row provider">
-            <div className="col-md-3 text-center py-5">
-                <h1 className='fw-bold font-monospace'>1500+</h1>
-                <h5 className='fw-bold font-monospace'>Happy Customers</h5>
-            </div>
-            <div className="col-md-3 text-center py-5">
-                <h1 className='fw-bold font-monospace'>100%</h1>
-                <h5 className='fw-bold font-monospace'>Satisfaction</h5>
-            </div>
-            <div className="col-md-3 text-center py-5">
-                <h1 className='fw-bold font-monospace'>1000+</h1>
-                <h5 className='fw-bold font-monospace'>Cleans Completed</h5>
-            </div>
-            <div className="col-md-3 text-center py-5">
-                <h1 className='fw-bold font-monospace'>50+</h1>
-                <h5 className='fw-bold font-monospace'>Cleaners</h5>
+        <div className="container">
+            <div className="row provider">
+                <div className="col-md-3 text-center py-5">
+                    <h1 className='fw-bold font-monospace'>1500+</h1>
+                    <h5 className='fw-bold font-monospace'>Happy Customers</h5>
+                </div>
+                <div className="col-md-3 text-center py-5">
+                    <h1 className='fw-bold font-monospace'>100%</h1>
+                    <h5 className='fw-bold font-monospace'>Satisfaction</h5>
+                </div>
+                <div className="col-md-3 text-center py-5">
+                    <h1 className='fw-bold font-monospace'>1000+</h1>
+                    <h5 className='fw-bold font-monospace'>Cleans Completed</h5>
+                </div>
+                <div className="col-md-3 text-center py-5">
+                    <h1 className='fw-bold font-monospace'>50+</h1>
+                    <h5 className='fw-bold font-monospace'>Cleaners</h5>
+                </div>
             </div>
         </div>
     );
