d3636945637e3a336bee393906453f16e07a93e8
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-12 20:37:22 +0600

    services added

--- src/components/Pages/Services/ServicesMain.js
+++ src/components/Pages/Services/ServicesMain.js
@@ -2,12 +2,14 @@ import React from 'react';
 import NavbarTop from '../Shared/NavbarTop/NavbarTop';
 import Footer from '../Shared/Footer/Footer';
 import Services from '../Home/Services/Services';
+import Overview from '../Home/Overview/Overview';
 
 
 const ServicesMain = () => {
     return (
         <div>
             <NavbarTop />
+            <Overview />
             <Services />
             <Footer />
         </div>
