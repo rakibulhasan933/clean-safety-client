8b420b191294934c64406a2e1418c91d3f4e9349
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-13 17:03:27 +0600

    header

--- src/components/Pages/Shared/NavbarTop/NavbarTop.js
+++ src/components/Pages/Shared/NavbarTop/NavbarTop.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './NavbarTop.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -11,6 +12,7 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const NavbarTop = () => {
     const { user, logout } = useAuth() || {};
+    console.log("navbar", user)
     return (
         <Navbar bg="light" variant="light">
             <Container>
@@ -29,11 +31,11 @@ const NavbarTop = () => {
                     <Nav.Link>
                         <Link className='text-decoration-none' to='/home'>About</Link>
                     </Nav.Link>
+                    {user.email ? <Button variant="outline-primary" onClick={logout}>Log out </Button> : <Button variant="outline-primary"><Link style={{ textDecoration: 'none' }} to='/login'>Login <FontAwesomeIcon icon={faArrowRight} /></Link></Button>}
                     <Nav.Link>
-                        <Link className='text-decoration-none' to='/home'>Contact us</Link>
+                        <Link className='text-decoration-none' to='/'><img className='profile-pic' src={user.photoURL} alt="profile" /></Link>
                     </Nav.Link>
-                    {user.email ? <Button variant="outline-primary"><Link style={{ textDecoration: 'none' }} to='/login'>Login <FontAwesomeIcon icon={faArrowRight} /></Link></Button> :
-                        <Button variant="outline-primary" onClick={logout}>Log out </Button>}
+
                 </Nav>
             </Container>
         </Navbar>
