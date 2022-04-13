480a88f01d581ebb7bac12ec6e2d4716b4efdd4e
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-14 00:37:29 +0600

    reviews

--- src/components/Pages/Dashboard/Reviews/Reviews.js
+++ src/components/Pages/Dashboard/Reviews/Reviews.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
+import useAuth from '../../../hooks/useAuth';
 
 const Reviews = () => {
     const {
@@ -9,16 +12,40 @@ const Reviews = () => {
         reset,
         formState: { errors },
     } = useForm();
+    const { user } = useAuth() || {};
+
+    const { displayName, photoURL } = user;
 
     const onSubmit = (data) => {
-        console.log(data);
+        const serviceData = {
+            name: displayName,
+            comments: data.comments,
+            imageURL: photoURL
+        }
+        // console.log(serviceData);
+        axios.post('http://localhost:5000/reviews', serviceData)
+            .then(res => {
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: 'Thank you so much for giving us feedback',
+                        showConfirmButton: false,
+                        timer: 2500
+                    })
+                    reset();
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     return (
         <Container>
 
             <h4 className="font-monospace fw-bold pt-5">Write Down Your Review</h4>
 
-            <hr className="w-75 mb-5 mx-auto" />
+            <hr className="w-100 mb-5 mx-auto" />
 
             <form onSubmit={handleSubmit(onSubmit)}>
                 <textarea
@@ -28,18 +55,6 @@ const Reviews = () => {
                     {...register("comments", { required: true })}
                 />
                 <br />
-
-                <select
-                    className=" w-20 mb-3"
-                    {...register("ratting")}>
-                    <option value="1">One Star</option>
-                    <option value="2">Two Star</option>
-                    <option value="3">There Star</option>
-                    <option value="4">Four Star</option>
-                    <option value="5">Five Star</option>
-                </select>
-                <br />
-
                 <input
                     className="submit-btn btn btn-info mt-4 mb-3"
                     type="submit"
