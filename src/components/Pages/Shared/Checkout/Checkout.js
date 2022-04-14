f91b176a9f4d8359080effaa58a0f9ce31dc1213
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-14 22:02:11 +0600

    checkout card style

--- src/components/Pages/Shared/Checkout/Checkout.js
+++ src/components/Pages/Shared/Checkout/Checkout.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import { useParams } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 import NavbarTop from '../NavbarTop/NavbarTop';
 
 const Checkout = () => {
+    const [service, setService] = useState({})
     const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setService(data);
+            })
+    }, [id]);
+    const { name, price, description, imageURL } = service;
+
     return (
         <Container>
             <NavbarTop />
-            <div className="py-3">
-                <h1>Hello Product   {id}</h1>
+            <div className="row p-3">
+                <div className="col-md-5">
+                    <h1> hello customer</h1>
+                </div>
+                <div className="col-md-5">
+                    <div className="card h-80 shadow-lg p-3 mb-5 bg-body rounded">
+                        <img src={imageURL} className="card-img-top img-fluid rounded " alt="carpet" />
+                        <div className="card-body ">
+                            <h4 className="card-title fw-bold">{name} </h4>
+                            <p className="card-text fw-lighter">{description}</p>
+                            <h5>Price: $ {price} </h5>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <Footer />
         </Container>
     );
 };
