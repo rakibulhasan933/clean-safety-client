9f18c28ae63bcdb65d94918c2a89cb1ed9845cfc
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-14 22:22:45 +0600

    cheackout

--- src/components/Pages/Shared/Checkout/Checkout.js
+++ src/components/Pages/Shared/Checkout/Checkout.js
@@ -21,14 +21,14 @@ const Checkout = () => {
     return (
         <Container>
             <NavbarTop />
-            <div className="row p-3">
-                <div className="col-md-5">
+            <div className="row py-5">
+                <div className="col-md-5 px-2">
                     <h1> hello customer</h1>
                 </div>
                 <div className="col-md-5">
                     <div className="card h-80 shadow-lg p-3 mb-5 bg-body rounded">
-                        <img src={imageURL} className="card-img-top img-fluid rounded " alt="carpet" />
-                        <div className="card-body ">
+                        <img src={imageURL} className="card-img-top img-fluid  rounded " alt="carpet" />
+                        <div className="card-body">
                             <h4 className="card-title fs-4 fw-bold font-monospace">{name} </h4>
                             <p className="card-text fw-lighter font-monospace text-muted">{description}</p>
                             <h5 className='fs-6 fw-bold font-monospace'>Price: $ {price} </h5>
