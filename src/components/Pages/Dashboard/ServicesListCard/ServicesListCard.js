af1abf8e573292e0d591b371da16904de79e0287
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-16 02:31:32 +0600

    list funcation

--- src/components/Pages/Dashboard/ServicesListCard/ServicesListCard.js
+++ src/components/Pages/Dashboard/ServicesListCard/ServicesListCard.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const ServicesListCard = ({ service }) => {
-
+    const { _id, name, description, imageURL, price } = service;
     return (
-        <div>
-
+        <div className="card h-100 shadow-lg  bg-body rounded">
+            <img src={imageURL} className="card-img-top" alt="carpet" />
+            <div className="card-body shadow-lg  bg-body rounded">
+                <h4 className="card-title fw-bold font-monospace">{name} </h4>
+                <h5 className="card-title fw-bold font-monospace">Price: $ {price} </h5>
+                <p className="card-text fw-lighter font-monospace">{description} </p>
+            </div>
+            <div className="card-footer shadow-lg  bg-body rounded">
+                <Button variant="outline-info fw-bold font-monospace">Deleted</Button>{' '}
+            </div>
         </div>
     );
 };
