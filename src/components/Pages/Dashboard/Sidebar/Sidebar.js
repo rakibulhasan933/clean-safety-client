b2b60c98b48961957ee2d4692019ff3ae149019f
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-15 20:39:38 +0600

    sidebar

--- src/components/Pages/Dashboard/Sidebar/Sidebar.js
+++ src/components/Pages/Dashboard/Sidebar/Sidebar.js
@@ -8,15 +8,16 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
     return (
-        <div className='m-1 sidebar'>
-            <div className="p-1 dashboard">
-                <h5 className='fw-bold font-monospace'>Dashboard</h5>
+        <div className='m-1 sidebar shadow-lg rounded'>
+            <div className="p-1 dashboard ">
+                <h5 className='fw-bold font-monospace text-center'>Dashboard</h5>
                 <ul className='list-unstyled mt-4 fw-bold font-monospace '>
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='addProducts'><li className='fw-bold font-monospace sidebar-item'> <FontAwesomeIcon icon={faUserPlus} /> Add products</li></Link>
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='reviews'><li className='fw-bold font-monospace sidebar-item'><FontAwesomeIcon icon={faStar} /> Reviews</li></Link>
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='makeAdmin'><li className='fw-bold font-monospace sidebar-item'><FontAwesomeIcon icon={faPersonCirclePlus} /> Make Admin</li></Link>
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='/dashboard'><li className='fw-bold font-monospace sidebar-item'><FontAwesomeIcon icon={faList} /> Product List</li></Link >
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='/dashboard'><li className='fw-bold font-monospace sidebar-item'> <FontAwesomeIcon icon={faSitemap} /> Oder List</li></Link >
+                    <Link style={{ textDecoration: 'none', color: 'blue' }} to='/dashboard'><li className='fw-bold font-monospace sidebar-item'> <FontAwesomeIcon icon={faSitemap} /> My Oder List</li></Link >
                 </ul>
             </div>
         </div>
