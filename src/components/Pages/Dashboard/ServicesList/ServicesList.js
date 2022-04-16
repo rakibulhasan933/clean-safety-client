abf2b09a0dbf368e7f91e364b009ef273ac347f0
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-16 12:18:07 +0600

    products list update

--- src/components/Pages/Dashboard/ServicesList/ServicesList.js
+++ src/components/Pages/Dashboard/ServicesList/ServicesList.js
@@ -1,15 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import ServicesListCard from '../ServicesListCard/ServicesListCard';
+import Button from 'react-bootstrap/Button';
+import Swal from 'sweetalert2';
+
 
 const ServicesList = () => {
-    const [services, setServices] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [isDelete, setIsDelete] = useState(null);
     useEffect(() => {
         fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => {
-                setServices(data);
+                setProducts(data);
             })
-    }, [])
+    }, [products, isDelete])
+    const handleDeleteProduct = (id) => {
+        console.log(id);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/deleteServices/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(result => {
+                        if (result.deletedCount) {
+
+                            Swal.fire(
+                                'Deleted!',
+                                'This product has been deleted.',
+                                'success'
+                            )
+
+                            const remainingUsers = products.filter(order => order._id !== id);
+                            setProducts(remainingUsers);
+                            setIsDelete(true);
+                        } else {
+                            setIsDelete(false);
+                        }
+                    })
+            }
+
+        })
+    };
     return (
         <div className='pb-3'>
             <h3 className='fw-bold font-monospace'>Your ordered service list and service status</h3>
@@ -17,7 +57,19 @@ const ServicesList = () => {
             <div className="container p-1">
                 <div className="row">
                     {
-                        services.length && services.map(service => <ServicesListCard key={service._id} service={service} />)
+                        products.map(service => <div className="col-md-4">
+                            <div className="card h-25 shadow-lg  bg-body rounded">
+                                <img src={service.imageURL} className="card-img-top" alt="carpet" />
+                                <div className="card-body shadow-lg  bg-body rounded">
+                                    <h4 className="card-title fw-bold font-monospace">{service.name} </h4>
+                                    <h5 className="card-title fw-bold font-monospace">Price: $ {service.price} </h5>
+                                    <p className="card-text fw-lighter font-monospace">{service.description} </p>
+                                </div>
+                                <div className="card-footer shadow-lg  bg-body rounded">
+                                    <Button onClick={() => handleDeleteProduct(service._id)} variant="outline-info fw-bold font-monospace">Deleted</Button>
+                                </div>
+                            </div>
+                        </div>)
                     }
                 </div>
             </div>
