af844947a688038363c1aad80c2024cac337c024
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-16 12:32:59 +0600

    sidebar update

--- src/components/Pages/Dashboard/Sidebar/Sidebar.js
+++ src/components/Pages/Dashboard/Sidebar/Sidebar.js
@@ -16,7 +16,7 @@ const Sidebar = () => {
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='reviews'><li className='fw-bold font-monospace sidebar-item'><FontAwesomeIcon icon={faStar} /> Reviews</li></Link>
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='makeAdmin'><li className='fw-bold font-monospace sidebar-item'><FontAwesomeIcon icon={faPersonCirclePlus} /> Make Admin</li></Link>
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='servicesList'><li className='fw-bold font-monospace sidebar-item'><FontAwesomeIcon icon={faList} /> Product List</li></Link >
-                    <Link style={{ textDecoration: 'none', color: 'blue' }} to='/dashboard'><li className='fw-bold font-monospace sidebar-item'> <FontAwesomeIcon icon={faSitemap} /> Oder List</li></Link >
+                    <Link style={{ textDecoration: 'none', color: 'blue' }} to='/oder'><li className='fw-bold font-monospace sidebar-item'> <FontAwesomeIcon icon={faSitemap} /> Oder List</li></Link >
                     <Link style={{ textDecoration: 'none', color: 'blue' }} to='/dashboard'><li className='fw-bold font-monospace sidebar-item'> <FontAwesomeIcon icon={faSitemap} /> My Oder List</li></Link >
                 </ul>
             </div>
