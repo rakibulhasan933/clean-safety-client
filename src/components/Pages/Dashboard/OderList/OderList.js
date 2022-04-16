27d1ee507edfb99fd3612212d2a594e74b071a5f
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-16 17:48:11 +0600

    order list deleted button

--- src/components/Pages/Dashboard/OderList/OderList.js
+++ src/components/Pages/Dashboard/OderList/OderList.js
@@ -1,13 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Swal from 'sweetalert2';
 
 const OderList = () => {
     const [oder, setOder] = useState([]);
+    const [isDelete, setIsDelete] = useState(null);
     useEffect(() => {
         fetch('http://localhost:5000/oder')
             .then(res => res.json())
             .then(data => setOder(data))
     }, [])
+    const handleDeleteOder = (id) => {
+        console.log(id);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/oder/delete/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(result => {
+                        if (result.deletedCount) {
+
+                            Swal.fire(
+                                'Deleted!',
+                                'This Oder has been deleted.',
+                                'success'
+                            )
+
+                            const remainingUsers = oder.filter(order => order._id !== id);
+                            setOder(remainingUsers);
+                            setIsDelete(true);
+                        } else {
+                            setIsDelete(false);
+                        }
+                    })
+            }
+
+        })
+    };
     return (
         <div className='py-2'>
             <div className="pb-2">
@@ -32,7 +71,7 @@ const OderList = () => {
                                 <td className='fw-bold font-monospace'>{od.phone} </td>
                                 <td className='fw-bold font-monospace'>{od.time} </td>
                                 <td className='fw-bold font-monospace'> {od.address} </td>
-                                <td><Button className='fw-bold font-monospace btn btn-danger' >Deleted</Button></td>
+                                <td><Button onClick={() => handleDeleteOder(od._id)} className='fw-bold font-monospace btn btn-danger'>Deleted</Button></td>
                             </tr>
                         </tbody>)
                     }
