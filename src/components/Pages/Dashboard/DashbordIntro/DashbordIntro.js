489e4c50c383b9aafd058dd649b3cc7cbbdeddf6
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-17 10:35:06 +0600

    dashboard style

--- src/components/Pages/Dashboard/DashbordIntro/DashbordIntro.js
+++ src/components/Pages/Dashboard/DashbordIntro/DashbordIntro.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './DashbordIntro.css';
+import useAuth from '../../../hooks/useAuth';
 
 const DashbordIntro = () => {
+    const { user } = useAuth() || {};
+    const { displayName, email } = user;
     return (
         <div className='container py-5' >
             <h1 className='text-center fs-1 font-monospace fw-bold' >Welcome to <span>Dashboard</span></h1>
+            <br />
+            <h4 className='text-center fs-1 font-monospace fw-bold' > {displayName} </h4>
+            <p className='text-center fs-1 font-monospace fw-bold'>Your Email:- {email} </p>
         </div>
     );
 };
