4a6f90eb041e2b1f41381b42815ef28dd5386e72
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-18 16:23:34 +0600

    spineer oder list

--- src/components/Pages/Services/Service/Service.js
+++ src/components/Pages/Services/Service/Service.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Overview from '../../Home/Overview/Overview';
+import Spinner from '../../Shared/Spinner/Spinner';
 import ServiceCard from '../ServiceCard/ServiceCard';
 import './Service.css';
 
@@ -18,11 +19,13 @@ const Service = () => {
             <div className="container py-3">
                 <Overview />
                 <div className="container py-2">
-                    <div className="service-container">
-                        {
-                            service.map(item => <ServiceCard key={item._id} item={item} ></ServiceCard>)
-                        }
-                    </div>
+                    <Suspense fallback={<Spinner />}>
+                        <div className="service-container">
+                            {
+                                service.map(item => <ServiceCard key={item._id} item={item} ></ServiceCard>)
+                            }
+                        </div>
+                    </Suspense>
                 </div>
             </div>
         </div>
