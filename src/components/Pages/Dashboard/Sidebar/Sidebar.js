ad6824aebf4eb839e66db0cac2b3951a66c2cbbf
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-21 03:19:21 +0600

    dashobard style

--- src/components/Pages/Dashboard/Sidebar/Sidebar.js
+++ src/components/Pages/Dashboard/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPersonCirclePlus, faStar, faUserPlus, faList, faSitemap } from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
