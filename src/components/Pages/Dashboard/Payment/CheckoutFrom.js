16428c55e36eeb8253f61b3fd929d68495f90f18
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-26 03:29:11 +0600

    payment stripe

--- src/components/Pages/Dashboard/Payment/CheckoutFrom.js
+++ src/components/Pages/Dashboard/Payment/CheckoutFrom.js
@@ -1,9 +1,14 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import './common.css';
 
 const CheckoutFrom = ({ service }) => {
+    const { price } = service;
     const stripe = useStripe();
     const elements = useElements();
+
+    const [error, setError] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -17,29 +22,46 @@ const CheckoutFrom = ({ service }) => {
         if (card === null) {
             return;
         }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+        if (error) {
+            // console.log(error);
+            setError(error.message);
+        }
+        else {
+            setError('');
+            console.log(paymentMethod);
+        }
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <div className="container p-2">
+            <form className='card-form' onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <Button style={{ width: '100px' }} className=' p-2' type="submit" disabled={!stripe}>
+                    Pay ${price}
+                </Button>
+            </form>
+            {
+                error && <p style={{ color: 'red' }}>{error}</p>
+            }
+        </div>
     );
 };
 
